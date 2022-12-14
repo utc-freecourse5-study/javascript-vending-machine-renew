0062309080ccc6e6ebba433f8d0cc3bf07a724a5
woo-jk <201702042@o.cnu.ac.kr>
2022-12-15 03:13:27 +0900

    feat: VendingMachine 코인 생성 함수 구현

--- src/VendingMachine.js
+++ src/VendingMachine.js
@@ -1,1 +1,39 @@
-class VendingMachine {}
+const RandomCoinGenerator = require("./RandomCoinGenerator");
+
+class VendingMachine {
+  #coins = {
+    COIN_500: 0,
+    COIN_100: 0,
+    COIN_50: 0,
+    COIN_10: 0,
+  };
+  #products = [];
+
+  constructor(money) {
+    this.generateCoins(money);
+  }
+
+  #insertCoin(coin) {
+    if (coin === 500) this.#coins.COIN_500 += 1;
+    if (coin === 100) this.#coins.COIN_100 += 1;
+    if (coin === 50) this.#coins.COIN_50 += 1;
+    if (coin === 10) this.#coins.COIN_10 += 1;
+  }
+
+  generateCoins(money) {
+    let currentMoney = 0;
+    while (currentMoney < money) {
+      let coin = RandomCoinGenerator.generate();
+      if (coin < money && coin + currentMoney <= money) {
+        currentMoney += coin;
+        this.#insertCoin(coin);
+      }
+    }
+  }
+
+  getCoins() {
+    console.log(this.#coins.COIN_500, this.#coins.COIN_100, this.#coins.COIN_50, this.#coins.COIN_10);
+  }
+}
+
+module.exports = VendingMachine;
