f8bfede9753b271001b76e67edc14d7115cfbee6
woo-jk <201702042@o.cnu.ac.kr>
2022-12-15 04:43:33 +0900

    feat: InputValidator 투입 금액 입력 검증 함수 구현

--- src/InputValidator.js
+++ src/InputValidator.js
@@ -14,6 +14,12 @@ const InputValidator = {
       throw new Error("[ERROR] 상품의 가격은 100원부터 시작해야 하며, 10원 단위로 나누어 떨어져야 합니다.");
     if (isNaN(product[2])) throw new Error("[ERROR] 상품의 개수는 숫자 형식으로 입력해야 합니다.");
   },
+
+  validateUserMoney(money) {
+    if (isNaN(money)) {
+      throw new Error("[ERROR] 투입 금액은 숫자 형식이어야 합니다.");
+    }
+  },
 };
 
 module.exports = InputValidator;
