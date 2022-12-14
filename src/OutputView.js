4cc7035169498e4dd536558f9fd94fd87dc4e34f
woo-jk <201702042@o.cnu.ac.kr>
2022-12-15 01:30:01 +0900

    feat: OutputView 출력 함수들 구현

--- src/OutputView.js
+++ src/OutputView.js
@@ -1,3 +1,25 @@
-const OutputView = {};
+const MissionUtils = require("@woowacourse/mission-utils");
+
+const OutputView = {
+  printVendingMachineCoin(fiveHundred, oneHundred, fifty, ten) {
+    MissionUtils.Console.print("자판기가 보유한 동전");
+    MissionUtils.Console.print(`500원 - ${fiveHundred}개`);
+    MissionUtils.Console.print(`100원 - ${oneHundred}개`);
+    MissionUtils.Console.print(`50원 - ${fifty}개`);
+    MissionUtils.Console.print(`10원 - ${ten}개`);
+  },
+
+  printCurrentVendingMachineMoney(money) {
+    MissionUtils.Console.print(`투입 금액: ${money}원`);
+  },
+
+  printChanges(fiveHundred, oneHundred, fifty, ten) {
+    MissionUtils.Console.print("잔돈");
+    fiveHundred && MissionUtils.Console.print(`500원 - ${fiveHundred}개`);
+    oneHundred && MissionUtils.Console.print(`100원 - ${oneHundred}개`);
+    fifty && MissionUtils.Console.print(`50원 - ${fifty}개`);
+    ten && MissionUtils.Console.print(`10원 - ${ten}개`);
+  },
+};
 
 module.exports = OutputView;
