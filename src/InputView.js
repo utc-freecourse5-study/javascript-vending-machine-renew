aabac5f43ce109b2b2b70b16056d2333c96f169a
woo-jk <201702042@o.cnu.ac.kr>
2022-12-15 01:20:37 +0900

    feat: InputView 입력 함수들 구현

--- src/InputView.js
+++ src/InputView.js
@@ -1,3 +1,21 @@
-const InputView = {};
+const MissionUtils = require("@woowacourse/mission-utils");
+
+const InputView = {
+  readVendingMachineMoney(callback) {
+    MissionUtils.Console.readLine("자판기가 보유하고 있는 금액을 입력해 주세요.\n", callback);
+  },
+
+  readProductList(callback) {
+    MissionUtils.Console.readLine("상품명과 가격, 수량을 입력해 주세요.\n", callback);
+  },
+
+  readUserMoney(callback) {
+    MissionUtils.Console.readLine("투입 금액을 입력해 주세요.\n", callback);
+  },
+
+  readProductName(callback) {
+    MissionUtils.Console.readLine("구매할 상품명을 입력해 주세요.\n", callback);
+  },
+};
 
 module.exports = InputView;
