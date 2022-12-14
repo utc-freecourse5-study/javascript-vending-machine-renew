04005be1336458d2e5a4d5c7f731292feff3ff83
woo-jk <201702042@o.cnu.ac.kr>
2022-12-15 04:08:04 +0900

    fix: OutputView 동전 인자 객체로 받도록 수정

--- src/OutputView.js
+++ src/OutputView.js
@@ -1,8 +1,8 @@
 const MissionUtils = require("@woowacourse/mission-utils");
 
 const OutputView = {
-  printVendingMachineCoins(COIN_500, COIN_100, COIN_50, COIN_10) {
-    MissionUtils.Console.print("자판기가 보유한 동전");
+  printVendingMachineCoins({ COIN_500, COIN_100, COIN_50, COIN_10 }) {
+    MissionUtils.Console.print("\n자판기가 보유한 동전\n");
     MissionUtils.Console.print(`500원 - ${COIN_500}개`);
     MissionUtils.Console.print(`100원 - ${COIN_100}개`);
     MissionUtils.Console.print(`50원 - ${COIN_50}개`);
@@ -13,7 +13,7 @@ const OutputView = {
     MissionUtils.Console.print(`투입 금액: ${money}원`);
   },
 
-  printChanges(COIN_500, COIN_100, COIN_50, COIN_10) {
+  printChanges({ COIN_500, COIN_100, COIN_50, COIN_10 }) {
     MissionUtils.Console.print("잔돈");
     COIN_500 && MissionUtils.Console.print(`500원 - ${COIN_500}개`);
     COIN_100 && MissionUtils.Console.print(`100원 - ${COIN_100}개`);
