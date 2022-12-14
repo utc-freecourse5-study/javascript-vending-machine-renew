5c9d6f24545bf5b2eebcf2383b7b96a44f56ab39
woo-jk <201702042@o.cnu.ac.kr>
2022-12-15 04:35:31 +0900

    feat: InputValidator 상품 형식 검증 함수 구현

--- src/InputValidator.js
+++ src/InputValidator.js
@@ -7,6 +7,13 @@ const InputValidator = {
       throw new Error("[ERROR] 금액은 10원 단위로 입력해야 합니다.");
     }
   },
+
+  validateProductForm(product) {
+    if (isNaN(product[1])) throw new Error("[ERROR] 상품의 가격은 숫자 형식으로 입력해야 합니다.");
+    if (Number(product[1]) < 100 && Number(product[1]) % 10 !== 0)
+      throw new Error("[ERROR] 상품의 가격은 100원부터 시작해야 하며, 10원 단위로 나누어 떨어져야 합니다.");
+    if (isNaN(product[2])) throw new Error("[ERROR] 상품의 개수는 숫자 형식으로 입력해야 합니다.");
+  },
 };
 
 module.exports = InputValidator;
