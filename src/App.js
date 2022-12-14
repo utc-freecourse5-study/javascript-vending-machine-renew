e9f2c21315117b25f9ea854ac30134eef5337eeb
woo-jk <201702042@o.cnu.ac.kr>
2022-12-15 04:09:39 +0900

    feat: App 자판기 보유 금액 설정 과정 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,38 @@
+const InputValidator = require("./InputValidator");
+const InputView = require("./InputView");
+const OutputView = require("./OutputView");
+const VendingMachine = require("./VendingMachine");
+
 class App {
-  play() {}
+  #vendingMachine;
+
+  play() {
+    this.requestVendingMachineCoin();
+  }
+
+  requestVendingMachineCoin() {
+    InputView.readVendingMachineMoney((money) => {
+      if (!this.tryValidate(InputValidator.validateVendingMachineMoney, money)) {
+        this.requestVendingMachineCoin();
+        return;
+      }
+      this.#vendingMachine = new VendingMachine(money);
+      OutputView.printVendingMachineCoins(this.#vendingMachine.getCoinsCount());
+    });
+  }
+
+  tryValidate(validate, input) {
+    try {
+      validate(input);
+      return true;
+    } catch (error) {
+      OutputView.printErrorMessage(error);
+      return false;
+    }
+  }
 }
+
+const app = new App();
+app.play();
 
 module.exports = App;
