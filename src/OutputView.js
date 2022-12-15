d2614b940cf99b8cce543534950719699c2172f2
turtle601 <turtle601@naver.com>
2022-12-16 00:48:10 +0900

    feat(OutputView) 잔돈, 투입금액 출력 기능 구현

--- src/OutputView.js
+++ src/OutputView.js
@@ -1,3 +1,14 @@
-const OutputView = {};
+const { Console } = require('@woowacourse/mission-utils');
+const controller = require('./GameController');
+
+const OutputView = {
+  printChange() {
+    controller.outputChange().forEach((sentence) => Console.print(sentence));
+  },
+
+  printMoney() {
+    Console.print(controller.outputMoney());
+  },
+};
 
 module.exports = OutputView;
