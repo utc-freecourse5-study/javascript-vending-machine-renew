4d7d7446478ddbd74cebe6bc18c31be60369da58
turtle601 <turtle601@naver.com>
2022-12-16 00:08:37 +0900

    feat(InputException) 상품 입력 에러 체크 기능 구현

--- src/utils/InputException.js
+++ src/utils/InputException.js
@@ -1,9 +1,31 @@
 const { REGEX, ERROR_MESSAGE } = require('./constants');
 
 class InputException {
+  // -- change
   static checkChange(input) {
     if (!REGEX.divideTenNum.test(input)) {
       throw new Error(ERROR_MESSAGE.change);
+    }
+  }
+
+  // -- item
+  static isCorrectPrice(item) {
+    const price = item.split(',')[1];
+
+    return REGEX.divideTenNum.test(price) && Number(price) >= 100;
+  }
+
+  static isCorrectItems(input) {
+    const inputList = input.split(';');
+
+    return inputList.every((item) => {
+      return REGEX.item.test(item) && InputException.isCorrectPrice(item);
+    });
+  }
+
+  static checkItem(input) {
+    if (!InputException.isCorrectItems(input)) {
+      throw new Error(ERROR_MESSAGE.item);
     }
   }
 }
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,11 +1,12 @@
 const REGEX = Object.freeze({
   divideTenNum: /^\d*0$/,
-  // item: /^\[[가-힣a-z]*,(\d*0),(\d*)\]$/,
+  item: /^\[[가-힣a-z]*,(\d*0),(\d*)\]$/,
 });
 
 const ERROR_MESSAGE = Object.freeze({
   singleton: '[ERROR] 이 생성자 함수는 하나의 인스턴스만 생성 가능합니다.',
   change: '[ERROR] 금액은 숫자여야 하고 10으로 나눌 수도 있어야 합니다.',
+  item: '[ERROR] 상품명, 가격, 수량은 쉼표로, 개별 상품은 대괄호([])로 묶어 세미콜론(;)으로 구분, 상품 가격은 100원 부터 시작하며, 10원으로 나눠떨어져야한다.',
 });
 
 const MODEL_KEY = Object.freeze({
