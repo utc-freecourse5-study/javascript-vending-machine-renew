e032d110060442de4a5cbfc7fad7a560676eca5c
turtle601 <turtle601@naver.com>
2022-12-16 00:07:50 +0900

    test(validateTest) 상품 입력값 테스트 작성

--- __tests__/ValidateTest.js
+++ __tests__/ValidateTest.js
@@ -13,3 +13,19 @@ describe('(잔돈) InputException 입력값 테스트', () => {
     expect(() => InputException.checkChange(input)).not.toThrow();
   });
 });
+
+describe('(상품) InputException 입력값 테스트', () => {
+  test.each([
+    ['[콜라,1500,20];[사이다, 1000, 10]'],
+    ['[콜라,1500,20][사이다, 1000, 10]'],
+    ['0;[사이다,1000,10]'],
+    ['1000'],
+  ])('(실패) 숫자 입력값 확인', (input) => {
+    expect(() => InputException.checkItem(input)).toThrow('[ERROR]');
+  });
+
+  test('(성공) 재시작/종료 입력값 확인', () => {
+    const input = '[콜라,1500,20];[사이다,1000,10]';
+    expect(() => InputException.checkItem(input)).not.toThrow();
+  });
+});
