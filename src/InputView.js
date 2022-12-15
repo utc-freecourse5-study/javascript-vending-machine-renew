e408735762bfb403fd3d03084ea2761bf64c0fcb
turtle601 <turtle601@naver.com>
2022-12-15 22:59:44 +0900

    feat(InputView) 잔돈 입력 관련 기능 구현

--- src/InputView.js
+++ src/InputView.js
@@ -1,3 +1,37 @@
-const InputView = {};
+const { Console } = require('@woowacourse/mission-utils');
+
+const controller = require('./GameController');
+const OutputView = require('./OutputView');
+
+const { GAME_TEXT } = require('./utils/constants');
+const { errorCheckFor } = require('./utils/errorCheckFor');
+const InputException = require('./utils/InputException');
+
+const InputView = {
+  // -- ChangeInput
+  successVendingChangeEvent(input) {
+    InputException.checkChange(input);
+    controller.inputChange(input);
+    OutputView.printChange(input);
+
+    this.readVendingItem();
+  },
+
+  readVendingChangeEvent(input) {
+    errorCheckFor(
+      () => this.successVendingChangeEvent(input),
+      () => this.readVendingChange()
+    );
+  },
+
+  readVendingChange() {
+    Console.readLine(GAME_TEXT.change, (input) => {
+      this.readVendingChangeEvent(input);
+    });
+  },
+
+  // --ItemInput
+  readVendingItem() {},
+};
 
 module.exports = InputView;
